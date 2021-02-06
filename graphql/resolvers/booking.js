3b292235147405c5d0a35ec8a575fcd5232e369c
teammob <yucel.zengin@teammob.co.uk>
2021-02-06 01:31:43 +0000

    token  functions are completed

--- graphql/resolvers/booking.js
+++ graphql/resolvers/booking.js
@@ -3,7 +3,10 @@ const {  transformEvent ,transformBooking } = require('./merge');
 const Event= require('../../models/event');
 
 module.exports = {
-     bookings: async () => {
+     bookings: async (args, req) => {
+        if(!req.isAuth){
+            throw new Error('UnAuthenticated !');
+     }
         try{
             const bookings= await Booking.find();
             return bookings.map(booking => 
@@ -12,16 +15,22 @@ module.exports = {
         catch(err){ throw err; }
      }, 
      
-     bookEvent: async args =>{ 
+     bookEvent: async (args, req) =>{
+        if(!req.isAuth){
+            throw new Error('UnAuthenticated !');
+     } 
          const fetchedEvent = await Event.findOne({_id: args.eventId});
          const booking=  new Booking({
-                user: '601b5487a83ccea1566d8518',
+                user: req.userId,
                 event: fetchedEvent
          });
          const result = await booking.save();
          return transformBooking(result);
      },
-        cancelBooking: async args => {
+        cancelBooking: async (args, req) => {
+            if(!req.isAuth){
+                throw new Error('UnAuthenticated !');
+         }
             try{
                 const booking = await Booking.findById(args.bookingId).populate('event');
                 const event = transformEvent(booking.event); 
