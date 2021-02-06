001748cb16fababe7a8eb2a4c59a15acd957c311
teammob <yucel.zengin@teammob.co.uk>
2021-02-06 18:28:01 +0000

    Frontend with login from jwt

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ const mongoose = require('mongoose');
 const graphqlSchema= require('./graphql/schema/index');
 const graphqlResolvers= require('./graphql/resolvers/index');
 const isAuth = require('./middleware/is-auth');
+const { request } = require('express');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(isAuth);
 
+app.use( (req, res, next ) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+
+    if(req.method === 'OPTIONS'){
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 app.use('/graphql', graphqlHTTP({
         schema: graphqlSchema,      
@@ -20,7 +31,7 @@ app.use('/graphql', graphqlHTTP({
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.jnntv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
 .then(()=> {
-    app.listen(3000);
+    app.listen(8000);
 }).catch(err => {
     console.log(err);
 })
